9f97908823276afa3a001c8463d39deb2522c477
Lawrence Wu <llwu@llwu.me>
2016-10-14 15:11:30 -0400

    Prototype addition inversion

--- reverseflow/arrows/arrow.py
+++ reverseflow/arrows/arrow.py
@@ -7,8 +7,5 @@ class Arrow:
     def __init__(self):
         pass
 
-    def get_in_ports(self):
-        return self.in_ports
-
-    def get_out_ports(self):
-        return self.out_ports
+    def invert():
+        pass
