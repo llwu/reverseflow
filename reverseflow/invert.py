ee49868f9c67cc76c9442d0ef6a94f27e3097149
Zenna Tavares <zennatavares@gmail.com>
2016-10-14 15:41:22 -0400

    invert in example

--- reverseflow/invert.py
+++ reverseflow/invert.py
@@ -1,11 +1,13 @@
 from reverseflow.util.mapping import ImageBimap
 ## There are a few different things
 ## - The initial inversion
-## - The symbolic manipulation
-## - The learning phase
+# The symbolic manipulation
+# The learning phase
+
 
 def naive_invert(a: Arrow) -> Arrow:
     """Invert an arrow to construct a parametric inverse"""
 
-def param_reduce(a: Arrow) -> Arrow:
-    """Attempt to reduce the number of parameters by symbolic substitution"""
+
+# def param_reduce(a: Arrow) -> Arrow:
+#     """Attempt to reduce the number of parameters by symbolic substitution"""
