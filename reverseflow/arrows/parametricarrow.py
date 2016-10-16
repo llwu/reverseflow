aa4c60f1123750b2e5f7591a94f18e888d0d952a
Lawrence Wu <llwu@llwu.me>
2016-10-16 19:53:10 -0400

    Preliminary ParamArrow model

--- reverseflow/arrows/parametricarrow.py
+++ reverseflow/arrows/parametricarrow.py
@@ -1,12 +1,43 @@
-from reverseflow.arrows.arrow import Arrow
+from typing import Set
+
+from reverseflow.arrows.compositearrow import CompositeArrow
+from reverseflow.util.mapping import Bimap
+from reverseflow.arrows.port import InPort, ParamInPort
 
 
-class ParametricArrow(Arrow):
+class ParametricArrow(CompositeArrow):
     """Parametric arrow"""
 
-    def is_parametric() -> bool:
-        return True
-
     def __init__(self):
-        self.param_inport = []  # type: List[ParamInPort]
-        # self.error_outport = []  # type: List[ErrorOutPort]
+        self.in_ports = []  # type: List[InPort]
+        self.out_ports = []  # type: List[OutPort]
+        self.param_ports = []  # type: List[ParamPort]
+        self.edges = Bimap()  # type: Bimap[OutPort, InPort]
+
+    def add_params(self, params: Set[InPort]) -> None:
+        """
+        Moves some ports from in_ports[] to param_ports[].
+        """
+        i = len(self.param_ports)
+        for in_port in self.in_ports:
+            if in_port in params:
+                self.in_ports.remove(in_port)
+                inner_port = self.edges.fwd(in_port)
+                self.edges.remove(in_port, inner_port)
+                param_port = ParamInPort(self, i)
+                self.param_ports.append(param_port)
+                self.edges.add(param_port, inner_port)
+                i += 1
+
+    """
+    TODO: copy this thing from compositearrow but make it for paramports instead
+    def get_boundary_outports(self) -> Set[OutPort]:
+        """
+        #Get the boundary outports
+        """
+        out_ports = set()  # type: Set[OutPort]
+        for (out_port, in_port) in self.edges.items():
+            if out_port.arrow is self:
+                out_ports.add(out_port)
+        return out_ports
+    """
