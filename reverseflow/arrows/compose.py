3c52765f02d86dec58a8bff5b292fab28ce53eca
minasyan <minasyan@mit.edu>
2016-10-18 07:48:20 -0400

    combine version of compose

--- reverseflow/arrows/compose.py
+++ reverseflow/arrows/compose.py
@@ -16,17 +16,26 @@ def compose(l: PrimitiveArrow, r: PrimitiveArrow) -> CompositeArrow:
                           edges=edges)
 
 @overload
-def compose(a: PrimitiveArrow, b: CompositeArrowa) -> CompositeArrow:
-    pass
+def compose_comb(l: Arrow, r: Arrow, out_to_in: List[int]) -> CompositeArrow:
+    in_ports = l.in_ports
+    out_ports = r.out_ports
 
-@overload
-def compose(a: PrimitiveArrow, b: CompositeArrow) -> CompositeArrow:
-    pass
+    r_in_connect = [False]*len(r.in_ports)
+    assert len(l.out_ports) == len(out_to_in), \
+        "Each out_port of l should have a r.in_port index to connect (or -1)"
 
-@overload
-def compose(a: CompositeArrow, b: PrimitiveArrow) -> CompositeArrow:
-    pass
+    edges = Bimap()
+    for i in range(len(l.out_ports)):
+        if out_to_in[i] == -1:
+            out_ports.append(l.out_ports[i])
+        else:
+            edges.add(l.out_ports[i], r.in_ports[out_to_in[i]])
+            r_in_connect[out_to_in[i]] = True
+    for i in range(len(r.in_ports)):
+        if not r_in_connect[i]:
+            in_ports.append(r.in_ports[i])
 
-@overload
-def compose(a: CompositeArrow, b: CompositeArrow) -> CompositeArrow:
-    pass
+    edges.update(l.edges)
+    edges.update(r.edges)
+    return CompositeArrow(in_ports = in_ports, out_ports = out_ports,
+                            edges = edges)
