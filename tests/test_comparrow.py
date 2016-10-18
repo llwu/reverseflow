3b5c9d6dfd02b3b64994dfc16a80075fb211a191
minasyan <minasyan@mit.edu>
2016-10-18 08:00:58 -0400

    Combine version of construct

--- tests/test_comparrow.py
+++ tests/test_comparrow.py
@@ -4,6 +4,7 @@ from reverseflow.arrows.primitive.math_arrows import MulArrow, AddArrow
 from reverseflow.arrows.primitive.control_flow_arrows import DuplArrow
 from reverseflow.util.mapping import Bimap
 from reverseflow.arrows.compositearrow import CompositeArrow, EdgeMap
+from reverseflow.arrows.compose import compose_comb
 from reverseflow.to_graph import arrow_to_graph
 from reverseflow.to_arrow import graph_to_arrow
 
@@ -19,6 +20,11 @@ def test_xyplusx() -> None:
     edges.add(mul.out_ports[0], add.in_ports[1])  # mul -> add
     d = CompositeArrow(in_ports=[dupl.in_ports[0], mul.in_ports[1]],
                        out_ports=[add.out_ports[0]], edges=edges)
+    # construct same composite arrow with compose_comb
+    dupl_to_mul = [0, -1]
+    c1 = compose_comb(dupl, mul, dupl_to_mul)
+    c1_to_add = [0, 1]
+    d1 = compose_comb(c1, add, c1_to_add)
     # import pdb; pdb.set_trace()
     tf_d = arrow_to_graph(d)
     d_2 = graph_to_arrow(tf_d)
