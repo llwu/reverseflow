3a31e591c2bfa9405f29c72029415685128d2edb
Zenna Tavares <zennatavares@gmail.com>
2016-10-20 13:53:28 -0400

    rewrite to_arrow, still broken

--- reverseflow/to_arrow.py
+++ reverseflow/to_arrow.py
@@ -1,6 +1,6 @@
 """Convert a tensoflow graph into an arrow"""
 from typing import List
-from tensorflow import Tensor, Graph, Operation
+from tensorflow import Tensor, Graph, Operation, Tuple
 from reverseflow.arrows.arrow import Arrow
 from reverseflow.arrows.compositearrow import CompositeArrow, EdgeMap
 from reverseflow.arrows.primitive.math_arrows import AddArrow, MulArrow
@@ -15,7 +15,7 @@ THE_DICT = {'Add': AddArrow,
             'Mul': MulArrow}
 
 
-def arrow_from_op(op: Operation) -> Arrow:
+def create_arrow_from_op(op: Operation) -> Arrow:
     """Construct arrow which corresponds to op"""
     return THE_DICT[op.type]()
 
@@ -30,18 +30,54 @@ def consumer_index(op: Operation, tensor: Tensor) -> int:
     """If op is the ith consumer of tensor, return i"""
     return pos_in_seq(op, tensor.consumers())
 
+def find_out_port(tensor: Tensor):
+    if len(tensor.consumers()) > 1:
+        ...
+    else:
+        ...
+
 
 @overload
-def graph_to_arrow(graph: Graph) -> Arrow:
+def graph_to_arrow(graph: Graph, output_tensors: List[Tensor]) -> Arrow:
     """Convert a tensorflow graph into an arrow"""
+    edges = Bimap()  # type: EdgeMap
+    arrow_to_op = Bimap()  # type: Bimap[Arrow, Operation]
+    op_to_arrow = Bimap()  # type: Bimap[Operation, Arrow]
+    tensor_to_dupl = Bimap()  # type: Bimap[Tensor, DuplArrow]
+    dupl_value_index = Dict() # type: Dict[DuplArrow, int]
+    comp_out_ports = []  # type: List[OutPort]
+    comp_in_ports = []  # type: List[InPort]
+    to_link = []  # type: List[Tuple(InPort, Tensor)]
+
+    for tensor in output_tensors:
+        # TODO: Allow more than zero consumers for output tensor
+        assert len(op.consumers()) > 0, "Output tensor can have no consumers"
+        op = tensor.op
+        if op in op_to_arrow:
+            arrow = op_to_arrow[op]
+        else:
+            arrow = create_arrow_from_op(op)
+
+        comp_out_ports.append(arrow.out_ports[tensor.value_index])
+        assert len(arrow.in_ports) == len(op.inputs())
+        for i in range(len(arrow.in_ports)):
+            to_link.append((in_port[s], op.inputs[i]))
+
+    assert len(to_link) > 0, "Expected nonzero number of inputs"
+    for in_port, inp_tensor in to_link:
+        out_port = find_outport(inp_tensor)
+        edges.add(outport, in_port)
+
+    return CompositeArrow(edges, out_ports=comp_out_ports,
+                          in_ports=comp_in_ports)
+
+
+
+
     # TODO: Should we infer in_ports or should we chose them or both
     # TODO: How to add duplication to the list
     # TODO: Handle out_ports
 
-    edges = Bimap()  # type: EdgeMap
-    ops = graph.get_operations()
-    arrow_to_op = Bimap()  # type: Bimap[Arrow, Operation]
-    tensor_to_dupl = Bimap()  # type: Bimap[Tensor, DuplArrow]
     in_ports = []
     out_ports = []
 
