807c995d5965541ebfb27cb27ffa6dc290e3d118
Zenna Tavares <zennatavares@gmail.com>
2016-10-20 18:18:24 -0400

    approximate arrow

---
+++ reverseflow/arrows/approximatearrow.py
@@ -0,0 +1,17 @@
+from typing import List
+from reverseflow.arrows.compositearrow import CompositeArrow, EdgeMap
+from reverseflow.arrows.port import InPort, OutPort, ErrorPort
+
+
+class ApproximateArrow(CompositeArrow):
+    """Approximate arrow
+    Has an addition error output
+    """
+
+    def __init__(self,
+                 edges: EdgeMap,
+                 in_ports: List[InPort],
+                 out_ports: List[OutPort],
+                 error_ports: List[ErrorPort]):
+        super().__init__(edges=edges, in_ports=in_ports, out_ports=out_ports)
+        self.error_ports = error_ports  # type: List[ErrorPort]
