e62bed1bc1339fadd1093f3382ab8471aa70b0f8
Zenna Tavares <zennatavares@gmail.com>
2016-10-24 13:11:31 -0400

    added init

---
+++ reverseflow/inv_primitives/__init__.py
@@ -0,0 +1,1 @@
+"""Inverse Arrows for primitive arrows"""
--- reverseflow/inv_primitives/inv_math_arrows.py
+++ reverseflow/inv_primitives/inv_math_arrows.py
@@ -1,13 +1,21 @@
+from typing import Tuple, Dict
 from reverseflow.arrows.arrow import Arrow
 from reverseflow.arrows.parametricarrow import ParametricArrow
 from reverseflow.arrows.port import InPort, OutPort, ParamPort
 from reverseflow.util.mapping import Bimap, ImageBimap, OneToMany, OneToManyList
 from reverseflow.arrows.primitive.math_arrows import (AddArrow, SubArrow,
-    LogArrow)
+    MulArrow, DivArrow, ExpArrow, NegArrow, LogArrow)
 from reverseflow.arrows.primitive.control_flow_arrows import DuplArrow
+
+# Typealias
+ArrowInvRel = ImageBimap[Arrow, Tuple(Arrow, Dict[int, int])]
 
 
 class InvAddArrow(ParametricArrow):
+    """
+    Parametric Inverse Addition
+    add-1(z; theta) = (z-theta, theta)
+    """
 
     def __init__(self):
         name = "InvAdd"
@@ -26,13 +34,17 @@ class InvAddArrow(ParametricArrow):
                          param_ports=param_ports,
                          name=name)
 
-InvAddArrowSet = OneToManyList()  # type: ImageBimap[Arrow, Tuple(Arrow, Dict[Int, Int])]
+InvAddArrowSet = OneToManyList()  # type: ArrowInvRel
 InvAddArrowSet.add(AddArrow, (InvAddArrow, {}))
 InvAddArrowSet.add(AddArrow, (SubArrow, {0: 1}))
 InvAddArrowSet.add(AddArrow, (SubArrow, {1: 1}))
 
 
 class InvSubArrow(ParametricArrow):
+    """
+    Parametric Inverse Subtraction
+    sub-1(z; theta) = (z+theta, theta)
+    """
     def __init__(self):
         name = "InvSub"
         edges = Bimap()  # type: EdgeMap
@@ -46,7 +58,7 @@ class InvSubArrow(ParametricArrow):
                          out_ports=out_ports, param_ports=param_ports,
                          name=name)
 
-InvSubArrowSet = OneToManyList()
+InvSubArrowSet = OneToManyList()  # type: ArrowInvRel
 InvSubArrowSet.add(SubArrow, (InvSubArrow, {}))
 InvSubArrowSet.add(SubArrow, (AddArrow, {0: 0}))
 InvSubArrowSet.add(AddArrow, (SubArrow, {1: 0}))
@@ -69,11 +81,33 @@ class InvMulArrow(ParametricArrow):
                          param_ports=param_ports,
                          name=name)
 
-InvMulArrowSet = OneToManyList()  # type: ImageBimap[Arrow, Tuple(Arrow, Dict[Int, Int])]
+InvMulArrowSet = OneToManyList()  # type: ArrowInvRel
 InvMulArrowSet.add(MulArrow, (InvMulArrow, {}))
 InvMulArrowSet.add(MulArrow, (SubArrow, {0: 1}))
 InvMulArrowSet.add(MulArrow, (SubArrow, {1: 1}))
 
+
+class InvDivArrow(ParametricArrow):
+
+    def __init__(self):
+        name = "InvDivAdd"
+        edges = Bimap()  # type: EdgeMap
+        dupl_theta = DuplArrow()
+        div = DivArrow()
+
+        in_ports = div.in_ports[0]
+        out_ports = [div.out_ports[0], dupl_theta.out_ports[1]]
+        param_ports = [dupl_theta.in_ports[0]]
+        edges.add(dupl_theta.out_ports[0], div.in_ports[1])
+        super().__init__(edges=edges,
+                         in_ports=in_ports,
+                         out_ports=out_ports,
+                         param_ports=param_ports,
+                         name=name)
+
+InvMulArrowSet.add(MulArrow, (InvMulArrow, {}))
+InvMulArrowSet.add(MulArrow, (SubArrow, {0: 1}))
+InvMulArrowSet.add(MulArrow, (SubArrow, {1: 1}))
 
 
 class InvExpArrow(ParametricArrow):
@@ -93,24 +127,3 @@ class InvExpArrow(ParametricArrow):
                          out_ports=out_ports,
                          param_ports=param_ports,
                          name=name)
-
-    @staticmethod
-    def inverse_of() -> Arrow:
-        """What is this the inverse of"""
-        return ExpArrow
-
-    def procedure(self):
-        # consider having theta be something other than an InPort
-        edges = Bimap() #type: EdgeMap
-        dupl_theta = DuplArrow()
-        log = LogArrow()
-
-        in_ports = log.in_ports[1]
-        out_ports = [dupl_theta.out_ports[1], log.out_ports[0]]
-        param_ports = [dupl_theta.in_ports[0]]
-        edges.add(dupl_theta.out_ports[0], log.in_ports[0])
-        inv_exp = ParametricArrow(edges = edges,
-                                  in_ports = in_ports,
-                                  out_ports = out_ports,
-                                  param_ports = param_ports)
-        return inv_exp
