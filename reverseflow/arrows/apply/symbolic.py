7841e26eb96ebeff4d2e141c16179596ea9dce2c
Zenna Tavares <zennatavares@gmail.com>
2016-10-27 17:29:04 -0400

    added test arrow

--- reverseflow/arrows/apply/symbolic.py
+++ reverseflow/arrows/apply/symbolic.py
@@ -7,7 +7,6 @@ from reverseflow.arrows.primitive.math_arrows import (AddArrow,
     MulArrow, DivArrow)
 from reverseflow.arrows.primitive.control_flow_arrows import DuplArrow
 
-
 from pqdict import pqdict
 import sympy
 from sympy import Expr
