4f638787ab10f24b854a53d21243bd13edd21450
Lawrence Wu <llwu@llwu.me>
2016-10-27 17:59:37 -0400

    Add marking recursion

--- reverseflow/arrows/marking.py
+++ reverseflow/arrows/marking.py
@@ -1,10 +1,10 @@
 """Defines mark()."""
 
-from typing import Set
+from typing import Set, Tuple
 
 from pqdict import pqdict
 
-from reverseflow.arrows.port import InPort
+from reverseflow.arrows.port import InPort, OutPort
 from reverseflow.arrows.arrow import Arrow
 
 
@@ -30,6 +30,14 @@ def mark(arrow: Arrow, knowns: Set[InPort]) -> Tuple[Set[InPort], Set[OutPort]]:
         else:
             to_mark[sub_arrow] = sub_arrow.num_in_ports() - 1
 
+    def add(out_port):
+        """Marks out_port and (if it exists) the neighboring in port."""
+        marked_outports.add(out_port)
+        if out_port in arrow.edges.keys():
+            in_port = arrow.neigh_in_port(out_port)
+            marked_inports.add(in_port)
+            dec(in_port.arrow)
+
     for known in knowns:
         marked_inports.add(known)
         dec(known.arrow)
@@ -39,17 +47,15 @@ def mark(arrow: Arrow, knowns: Set[InPort]) -> Tuple[Set[InPort], Set[OutPort]]:
         assert priority >= 0, "knowns > num_in_ports?"
         if priority == 0:
             for out_port in sub_arrow.out_ports:
-                marked_outports.add(out_port)
-                if out_port in arrow.edges.keys():
-                    in_port = arrow.neigh_in_port(out_port)
-                    marked_inports.add(in_port)
-                    dec(in_port.arrow)
+                add(out_port)
         elif sub_arrow.is_composite:
             sub_knowns = set()
-            for in_port in sub_arrow.in_ports:
-                if in_port in marked_inports:
-                    sub_knowns.add(in_port)
+            for i in range(sub_arrow.num_in_ports()):
+                if InPort(sub_arrow, i) in marked_inports:  # outer InPort
+                    sub_knowns.add(sub_arrow.in_ports[i])  # inner InPort
             sub_marked_inports, sub_marked_outports = mark(sub_arrow, sub_knowns)
-
+            for i in range(sub_arrow.num_out_ports()):
+                if sub_arrow.out_ports[i] in sub_marked_outports:  # inner OutPort
+                    add(OutPort(sub_arrow, i))  # outer OutPort
 
     return marked_inports, marked_outports
