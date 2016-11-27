29da318f29797dcfaac008c545d704869e1c09af
Zenna Tavares <zennatavares@gmail.com>
2016-11-27 13:41:29 -0500

    unfinished training

---
+++ tests/test_train.py
@@ -0,0 +1,15 @@
+from reverseflow.train import train_y_arr, train_y_tf
+
+from test_arrows import test_xyplusx_flat
+from test_graphs import test_xy
+import numpy as np
+
+
+def test_train_graph():
+    graph, inputs, outputs = test_xyplusx_graph()
+    train_y_tf(outputs)
+
+def test_train_arrow():
+    arrow = test_xyplusx_flat()
+    dataset = np.random.rand(100)
+    train_y_arr(arrow, dataset)
