3e1ed0c401264d06e414abf74b2f5d89d527089d
Zenna Tavares <zennatavares@gmail.com>
2017-01-13 12:33:37 -0500

    required imports

--- arrows/apply/constants.py
+++ arrows/apply/constants.py
@@ -3,27 +3,38 @@ from arrows.port import Port
 from arrows.std_arrows import SourceArrow
 from arrows.arrow import Arrow
 from arrows.compositearrow import CompositeArrow
+from arrows.apply.propagate import propagate
 from overloading import overload
 from enum import Enum
+from typing import Dict
 
-class Const(Enum):
+
+class ValueType(Enum):
     CONSTANT = 0
     VARIABLE = 1
 
-PortKnowns = Dict[Port, Const]
+PortValues = Dict[Port, ValueType]
+
 
 @overload
-def sub_propagate(a: Arrow, port_to_known: PortKnowns, state=None):
+def sub_propagate(a: Arrow, port_to_known: PortValues, state=None):
+    pass
     # TODO:
     # If all the inputs are constant then outputs should be constant
     # If not all inputs are known then make no changed
     # If one input is variable then all outputs should be variable
 
+
 @overload
-def sub_propagate(a: SourceArrow, port_to_known: PortKnowns, state=None):
-    return {a.get_ports()[0]: Const.CONSTANT}
+def sub_propagate(a: SourceArrow, port_to_known: PortValues, state=None):
+    return {a.get_ports()[0]: ValueType.CONSTANT}
 
 
 @overload
-def sub_propagate(a: CompositeArrow, port_values: PortKnowns, state=None):
+def sub_propagate(a: CompositeArrow, port_values: PortValues, state=None):
     return propagate(sub_propagate, a, port_values, state=None)
+
+
+def propagate_constants(comp_arrow: CompositeArrow) -> PortValues:
+    """Propagate constants (originating in SourceArrows) around the graph"""
+    propagate(sub_propagate, comp_arrow, {}, {})
