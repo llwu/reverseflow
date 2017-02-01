2d736296105bf1ccb905adb51f9f96d9d882635d
minasyan <minasyan@mit.edu>
2017-02-01 16:34:04 -0500

    inverse kinematics stanford manipulator example

--- benchmarks/inversekinematics.py
+++ benchmarks/inversekinematics.py
@@ -13,14 +13,14 @@ s = tf.sin
 
 
 def ik_fwd_f(inputs):
-    phi1 = inputs['phi1']
-    phi2 = inputs['phi2']
-    phi4 = inputs['phi4']
-    phi5 = inputs['phi5']
-    phi6 = inputs['phi6']
-    d2 = inputs['d2']
-    d3 = inputs['d3']
-    h1 = inputs['h1']
+    phi1 = inputs[0]
+    phi2 = inputs[1]
+    phi4 = inputs[2]
+    phi5 = inputs[3]
+    phi6 = inputs[4]
+    d2 = inputs[5]
+    d3 = inputs[6]
+    h1 = inputs[7]
     r11 = -s(phi6)*(c(phi4)*s(phi1) + c(phi1)*c(phi2)*s(phi4) ) - c(phi6)*(c(phi5)*(s(phi1)*s(phi4) - c(phi1)*c(phi2)*c(phi4) ) + c(phi1)*s(phi2)*s(phi5) )
     r12 = s(phi6)*(c(phi5)*(s(phi1)*s(phi4) - c(phi1)*c(phi2)*c(phi4) ) + c(phi1)*s(phi2)*s(phi5) ) - c(phi6)*(c(phi4)*s(phi1) + c(phi1)*c(phi2)*s(phi4) )
     r13 = s(phi5)*(s(phi1)*s(phi4) - c(phi1)*c(phi2)*c(phi4) ) - c(phi1)*c(phi5)*s(phi2)
@@ -33,32 +33,32 @@ def ik_fwd_f(inputs):
     px = d2*s(phi1) - d3*c(phi1)*s(phi2)
     py = -d2*c(phi1) - d3*s(phi1)*s(phi2)
     pz = h1 + d3*c(phi2)
-    outputs = {'px': px, 'py': py,'pz': pz}
-    outputs.update({'r11': r11,
-                    'r12': r12,
-                    'r13': r13,
-                    'r21': r21,
-                    'r22': r22,
-                    'r23': r23,
-                    'r31': r31,
-                    'r32': r32,
-                    'r33': r33})
+    outputs = [px, py, pz]
+    outputs.extend([r11,
+                    r12,
+                    r13,
+                    r21,
+                    r22,
+                    r23,
+                    r31,
+                    r32,
+                    r33])
     return outputs
 
 
 def ik_gen_graph(g, batch_size, is_placeholder):
     with g.name_scope("fwd_g"):
-        inputs = {}
-        inputs['phi1'] = tf.placeholder(tf.float32, name="phi1", shape=(batch_size, 1))
-        inputs['phi2'] = tf.placeholder(tf.float32, name="phi2", shape=(batch_size, 1))
+        inputs = [0]*8
+        inputs[0] = tf.placeholder(tf.float32, name="phi1", shape=())
+        inputs[1] = tf.placeholder(tf.float32, name="phi2", shape=())
         # inputs['phi3'] = placeholder(tf.float32, name="phi3", shape=(batch_size, 1))
-        inputs['phi4'] = tf.placeholder(tf.float32, name="phi4", shape=(batch_size, 1))
-        inputs['phi5'] = tf.placeholder(tf.float32, name="phi5", shape=(batch_size, 1))
-        inputs['phi6'] = tf.placeholder(tf.float32, name="phi6", shape=(batch_size, 1))
+        inputs[2] = tf.placeholder(tf.float32, name="phi4", shape=())
+        inputs[3] = tf.placeholder(tf.float32, name="phi5", shape=())
+        inputs[4] = tf.placeholder(tf.float32, name="phi6", shape=())
 
-        inputs['d2'] = tf.placeholder(tf.float32, name="d2", shape=(batch_size, 1))
-        inputs['d3'] = tf.placeholder(tf.float32, name="d3", shape=(batch_size, 1))
-        inputs['h1'] = tf.placeholder(tf.float32, name="h1", shape=(batch_size, 1))
+        inputs[5] = tf.placeholder(tf.float32, name="d2", shape=())
+        inputs[6] = tf.placeholder(tf.float32, name="d3", shape=())
+        inputs[7] = tf.placeholder(tf.float32, name="h1", shape=())
 
         outputs = ik_fwd_f(inputs)
         return {"inputs": inputs, "outputs": outputs}
@@ -66,12 +66,14 @@ def ik_gen_graph(g, batch_size, is_placeholder):
 def test_ik():
     with tf.name_scope("ik_stanford_manipulator"):
         in_out = ik_gen_graph(tf.Graph(), 1, is_placeholder)
-        outputs = list(in_out["outputs"].values())
-    arrow = graph_to_arrow(outputs, name="ik_stanford")
+        input_values = [30, 45, 90, 0, 60, 1, 2, 1]
+        with tf.Session() as sess:
+            output_values = sess.run(in_out["outputs"], feed_dict={in_out["inputs"][i]: input_values[i] for i in range(len(input_values))})
+        print(output_values)
+    arrow = graph_to_arrow(in_out["outputs"], name="ik_stanford")
     show_tensorboard_graph()
     tf.reset_default_graph()
     inverse = invert(arrow)
-    output_values = [np.random.randn() for _ in range(len(outputs))]
     min_approx_error_arrow(inverse, output_values)
 
 test_ik()
