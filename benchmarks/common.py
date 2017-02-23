da5f38cc8d1b445aae0cff45e71d232322b7c6f3
minasyan <minasyan@mit.edu>
2017-02-22 22:28:36 -0500

    Running both stanford and linkage kinematics

--- benchmarks/common.py
+++ benchmarks/common.py
@@ -1,9 +1,21 @@
 """Functions common for examples"""
 import sys
 from arrows.util.io import *
+from linkage_kinematics import robo_tensorflow
+from stanford_kinematics import stanford_tensorflow
+from arrows.util.misc import rand_string, getn
+from metrics.generalization import test_everything
+from reverseflow.train.common import layer_width
+from reverseflow.train.reparam import *
+from reverseflow.train.unparam import unparam
+from reverseflow.train.loss import inv_fwd_loss_arrow, supervised_loss_arrow
+from reverseflow.train.supervised import supervised_train
+from reverseflow.train.callbacks import save_callback, save_options, save_every_n, save_everything_last
 
 import tensortemplates.res_net as res_net
 import tensortemplates.conv_res_net as conv_res_net
+import numpy as np
+import tensorflow as tf
 
 def stringy_dict(d):
     out = ""
@@ -69,3 +81,130 @@ def handle_options(name, argv):
     options = handle_args(argv, options)
     options['template'] = template_module[options['template']].template
     return options
+
+def robot_arm_arrow(batch_size, n_links, model_tensorflow):
+    angles, outputs = getn(model_tensorflow(batch_size, n_links), 'inputs', 'outputs')
+    arrow = graph_to_arrow(outputs,
+                           input_tensors=angles,
+                           name="robot_fwd_kinematics")
+    return arrow
+
+def gen_data(batch_size, n_links, model_tensorflow):
+    """Generate data for training"""
+    graph = tf.Graph()
+    with graph.as_default():
+        inputs, outputs = getn(model_tensorflow(batch_size, n_links), 'inputs', 'outputs')
+        input_data = [np.random.rand(batch_size, 1) for i in range(n_links)]
+        sess = tf.Session()
+        output_data = sess.run(outputs, feed_dict=dict(zip(inputs, input_data)))
+        sess.close()
+    return {'inputs': input_data, 'outputs': output_data}
+
+def pi_supervised(options):
+    """Neural network enhanced Parametric inverse! to do supervised learning"""
+    tf.reset_default_graph()
+    n_links = options['n_links']
+    batch_size = options['batch_size']
+    model_tensorflow = options['model']
+    arrow = robot_arm_arrow(batch_size, n_links, model_tensorflow)
+    inv_arrow = inv_fwd_loss_arrow(arrow)
+    right_inv = unparam(inv_arrow)
+    sup_right_inv = supervised_loss_arrow(right_inv)
+    # Get training and test_data
+    train_data = gen_data(batch_size, n_links, model_tensorflow)
+    test_data = gen_data(batch_size, n_links, model_tensorflow)
+
+    # Have to switch input from output because data is from fwd model
+    train_input_data = train_data['outputs']
+    train_output_data = train_data['inputs']
+    test_input_data = test_data['outputs']
+    test_output_data = test_data['inputs']
+    num_params = get_tf_num_params(right_inv)
+    print("Number of params", num_params)
+    supervised_train(sup_right_inv,
+                     train_input_data,
+                     train_output_data,
+                     test_input_data,
+                     test_output_data,
+                     callbacks=[save_every_n, save_everything_last, save_options],
+                     options=options)
+
+
+def nn_supervised(options):
+    """Plain neural network to do supervised learning"""
+    tf.reset_default_graph()
+    n_links = options['n_links']
+    n_outputs = options['n_outputs']
+    batch_size = options['batch_size']
+    model_tensorflow = options['model']
+    # Get training and test_data
+    train_data = gen_data(batch_size, n_links, model_tensorflow)
+    test_data = gen_data(batch_size, n_links, model_tensorflow)
+
+    # Have to switch input from output because data is from fwd model
+    train_input_data = train_data['outputs']
+    train_output_data = train_data['inputs']
+    test_input_data = test_data['outputs']
+    test_output_data = test_data['inputs']
+
+    template = res_net.template
+    n_layers = 2
+    l = round(max(*layer_width(2, n_links, n_layers, 630))) * 2
+    tp_options = {'layer_width': l,
+                  'num_layers': 2,
+                  'nblocks': 1,
+                  'block_size': 1,
+                  'reuse': False}
+
+    tf_arrow = TfArrow(n_outputs, n_links, template=template, options=tp_options)
+    for port in tf_arrow.ports():
+        set_port_shape(port, (None, 1))
+    sup_tf_arrow = supervised_loss_arrow(tf_arrow)
+    num_params = get_tf_num_params(sup_tf_arrow)
+    print("NNet Number of params", num_params)
+    supervised_train(sup_tf_arrow,
+                     train_input_data,
+                     train_output_data,
+                     test_input_data,
+                     test_output_data,
+                     callbacks=[save_every_n, save_everything_last, save_options],
+                     options=options)
+
+
+# Benchmarks
+def nn_benchmarks(model_name):
+    options = handle_options(model_name, sys.argv[1:])
+    # options['batch_size'] = np.round(np.logspace(0, np.log10(500-1), 10)).astype(int)
+    options['batch_size'] = 128
+    options['error'] = ['error']
+    if model_name == 'linkage_kinematics':
+        options['description'] = "Neural Network Linkage Generalization Benchmark"
+        options['n_links'] = 3
+        options['model'] = robo_tensorflow
+        options['n_outputs'] = 2
+    if model_name == 'stanford_kinematics':
+        options['description'] = "Neural Network Linkage Generalization Benchmark"
+        options['n_links'] = 6
+        options['model'] = stanford_tensorflow
+        options['n_outputs'] = 12
+    options['save'] = True
+    prefix = rand_string(5)
+    test_everything(nn_supervised, options, ["batch_size", "error"], prefix=prefix)
+
+
+def all_benchmarks(model_name):
+    options = handle_options(model_name, sys.argv[1:])
+    # options['batch_size'] = np.round(np.logspace(0, np.log10(500-1), 10)).astype(int)
+    options['batch_size'] = 128
+    options['error'] = ['supervised_error', 'inv_fwd_error', 'error', 'sub_arrow_error']
+    if model_name == 'linkage_kinematics':
+        options['description'] = "Parametric Inverse Linkage Generalization Benchmark"
+        options['n_links'] = 3
+        options['model'] = robo_tensorflow
+    if model_name == 'stanford_kinematics':
+        options['description'] = "Parametric Inverse Linkage Generalization Benchmark"
+        options['n_links'] = 6
+        options['model'] = stanford_tensorflow
+    options['save'] = True
+    prefix = rand_string(5)
+    test_everything(pi_supervised, options, ["batch_size", "error"], prefix=prefix)
--- benchmarks/stanford_kinematics.py
+++ benchmarks/stanford_kinematics.py
@@ -12,6 +12,7 @@ from reverseflow.to_arrow import graph_to_arrow
 from reverseflow.train.train_y import min_approx_error_arrow
 from reverseflow.train.loss import inv_fwd_loss_arrow
 from reverseflow.train.reparam import *
+from common import *
 
 
 plt.ion()
@@ -19,7 +20,7 @@ c = tf.cos
 s = tf.sin
 
 
-def ik_fwd_f(inputs):
+def stanford_fwd(inputs):
     phi1 = inputs[0]
     phi2 = inputs[1]
     phi4 = inputs[2]
@@ -127,74 +128,81 @@ def plot_3dline(x1, y1, z1, x2, y2, z2, ax):
     ax.plot(x, y, z, label='robot_arm')
 
 
-def ik_gen_graph(g, batch_size):
-    with g.name_scope("fwd_g"):
-        inputs = [0]*6
-        inputs[0] = tf.placeholder(tf.float32, name="phi1", shape=(batch_size, 1))
-        inputs[1] = tf.placeholder(tf.float32, name="phi2", shape=(batch_size, 1))
+def stanford_tensorflow(batch_size, n_links):
+    with tf.name_scope("fwd_stanford"):
+        inputs = []
+        for _ in range(n_links):
+            inputs.append(tf.placeholder(floatX(),
+                                         name="angle",
+                                         shape=(batch_size, 1)))
+        # inputs[0] = tf.placeholder(tf.float32, name="phi1", shape=(batch_size, 1))
+        # inputs[1] = tf.placeholder(tf.float32, name="phi2", shape=(batch_size, 1))
         # inputs['phi3'] = placeholder(tf.float32, name="phi3", shape=(batch_size, 1))
-        inputs[2] = tf.placeholder(tf.float32, name="phi4", shape=(batch_size, 1))
-        inputs[3] = tf.placeholder(tf.float32, name="phi5", shape=(batch_size, 1))
-        inputs[4] = tf.placeholder(tf.float32, name="phi6", shape=(batch_size, 1))
+        # inputs[2] = tf.placeholder(tf.float32, name="phi4", shape=(batch_size, 1))
+        # inputs[3] = tf.placeholder(tf.float32, name="phi5", shape=(batch_size, 1))
+        # inputs[4] = tf.placeholder(tf.float32, name="phi6", shape=(batch_size, 1))
 
-        inputs[5] = tf.placeholder(tf.float32, name="d2", shape=(batch_size, 1))
+        # inputs[5] = tf.placeholder(tf.float32, name="d2", shape=(batch_size, 1))
         # inputs[6] = tf.placeholder(tf.float32, name="d3", shape=())
         # inputs[7] = tf.Variable(1.0, name="h1")
 
-        outputs = ik_fwd_f(inputs)
-        return {"inputs": inputs, "outputs": outputs}
+        outputs = stanford_fwd(inputs)
+    return {"inputs": inputs, "outputs": outputs}
+
+if __name__ == '__main__':
+    all_benchmarks(model_name='stanford_kinematics')
 
 
-def test_ik():
-    batch_size = 6
-    with tf.name_scope("ik_stanford_manipulator"):
-        in_out = ik_gen_graph(tf.Graph(), batch_size)
-        train_input = [[[30]], [[45]], [[90]], [[0]], [[60]], [[1]]]
-        test_input = train_input
-        train_input = [np.random.rand(batch_size, 1)*90 for i in range(6)]
-        test_input = [np.random.rand(batch_size, 1)*90 for i in range(6)]
-        with tf.Session() as sess:
-            feed_dict = {in_out["inputs"][i]: train_input[i] for i in range(len(train_input))}
-            inv_train_data = sess.run(in_out["outputs"], feed_dict=feed_dict)
-            feed_dict = {in_out["inputs"][i]: test_input[i] for i in range(len(train_input))}
-            inv_test_data = sess.run(in_out["outputs"], feed_dict=feed_dict)
-
-    # plot_robot_arm(train_input, (output_values[0], output_values[1], output_values[2]))
-    arrow = graph_to_arrow(output_tensors=in_out["outputs"],
-                           input_tensors=in_out["inputs"],
-                           name="ik_stanford")
-    # show_tensorboard_graph()
-    tf.reset_default_graph()
-    # inverse = invert(arrow)
-    inv_arrow = inv_fwd_loss_arrow(arrow)
-    phi_shape = (batch_size, 6) # 6 DOF Robot so why not
-    rep_arrow = reparam(inv_arrow, phi_shape)
-    # target = (output_values[0], output_values[1], output_values[2])
-    def plot_callback(fetch_res):
-        import pdb; pdb.set_trace()
-        return 10
-        # robot_joints = fetch_res['output_tensors'][0:6]
-        # r = np.array(robot_joints).flatten()
-        # fig.clear()
-        # plot_robot_arm(list(r), target)
-        # plt.pause(0.01)
-        #
-
-    d = [p for p in inv_arrow.out_ports() if not is_error_port(p)]
-    reparam_arrow(rep_arrow,
-                  d,
-                  inv_train_data,
-                  inv_test_data,
-                  error_filter=lambda port: has_port_label(port, "inv_fwd_error"),
-                #   error_filter="inv_fwd_error",
-                  batch_size=batch_size,
-                  output_callback=plot_callback,
-                  debug=False)
-
-    min_approx_error_arrow(inv_fwd_arrow,
-                           output_values,
-                           error_filter=lambda port: has_port_label(port, "inv_fwd_error"),
-                           output_callback=plot_callback,
-                           debug=True)
-
-test_ik()
+# def test_ik():
+#     batch_size = 6
+#     with tf.name_scope("ik_stanford_manipulator"):
+#         in_out = ik_gen_graph(tf.Graph(), batch_size)
+#         train_input = [[[30]], [[45]], [[90]], [[0]], [[60]], [[1]]]
+#         test_input = train_input
+#         train_input = [np.random.rand(batch_size, 1)*90 for i in range(6)]
+#         test_input = [np.random.rand(batch_size, 1)*90 for i in range(6)]
+#         with tf.Session() as sess:
+#             feed_dict = {in_out["inputs"][i]: train_input[i] for i in range(len(train_input))}
+#             inv_train_data = sess.run(in_out["outputs"], feed_dict=feed_dict)
+#             feed_dict = {in_out["inputs"][i]: test_input[i] for i in range(len(train_input))}
+#             inv_test_data = sess.run(in_out["outputs"], feed_dict=feed_dict)
+#
+#     # plot_robot_arm(train_input, (output_values[0], output_values[1], output_values[2]))
+#     arrow = graph_to_arrow(output_tensors=in_out["outputs"],
+#                            input_tensors=in_out["inputs"],
+#                            name="ik_stanford")
+#     # show_tensorboard_graph()
+#     tf.reset_default_graph()
+#     # inverse = invert(arrow)
+#     inv_arrow = inv_fwd_loss_arrow(arrow)
+#     phi_shape = (batch_size, 6) # 6 DOF Robot so why not
+#     rep_arrow = reparam(inv_arrow, phi_shape)
+#     # target = (output_values[0], output_values[1], output_values[2])
+#     def plot_callback(fetch_res):
+#         import pdb; pdb.set_trace()
+#         return 10
+#         # robot_joints = fetch_res['output_tensors'][0:6]
+#         # r = np.array(robot_joints).flatten()
+#         # fig.clear()
+#         # plot_robot_arm(list(r), target)
+#         # plt.pause(0.01)
+#         #
+#
+#     d = [p for p in inv_arrow.out_ports() if not is_error_port(p)]
+#     reparam_arrow(rep_arrow,
+#                   d,
+#                   inv_train_data,
+#                   inv_test_data,
+#                   error_filter=lambda port: has_port_label(port, "inv_fwd_error"),
+#                 #   error_filter="inv_fwd_error",
+#                   batch_size=batch_size,
+#                   output_callback=plot_callback,
+#                   debug=False)
+#
+#     min_approx_error_arrow(inv_fwd_arrow,
+#                            output_values,
+#                            error_filter=lambda port: has_port_label(port, "inv_fwd_error"),
+#                            output_callback=plot_callback,
+#                            debug=True)
+#
+# test_ik()
