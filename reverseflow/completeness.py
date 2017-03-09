modified distances; tf.gradient bug

reverseflow/completeness.py
@@ -17,7 +17,7 @@ def norm(tensor):
 def circle_loss(tensor):
     s = 0.5
     norms = norm(tensor) + EPS
-    sdfs = tf.maximum(0.0, norms-s) + EPS
+    sdfs = tf.abs(norms-s) + EPS
     return tf.reduce_mean(sdfs) + EPS
 
 axis = plt.axis([-1, 1, -1, 1])
@@ -35,7 +35,7 @@ def gen_update_step(loss):
     with tf.name_scope('optimization'):
         # optimizer = tf.train.MomentumOptimizer(0.001,
         #                                        momentum=0.05)
-        optimizer = tf.train.AdamOptimizer(0.001)
+        optimizer = tf.train.AdamOptimizer(0.002)
         update_step = optimizer.minimize(loss)
         return update_step
 
@@ -66,12 +66,57 @@ def non_iden(permutation):
 def non_iden_idx(permutation):
     return [i for i, p in enumerate(permutation) if i != p]
 
+def non_equal(x, y):
+    assert len(x) == len(y)
+    new_x = []
+    new_y = []
+    for i in range(len(x)):
+        if x[i] != y[i]:
+            new_x.append(x[i])
+            new_y.append(y[i])
+    return new_x, new_y
 
-def rnd_pairwise_min_dist(phi, g, permutation, permutation_idx):
+def rnd_nn_mean_dist(phi, g, shape):
+    op, params = g(phi)
+    sess = tf.InteractiveSession()
+    init = tf.initialize_all_variables()
+    sess.run(init)
+    theta_samples = op[0]
+    theta_samples = tf.reshape(theta_samples, shape)
+    # theta_samples[0].eval(session=sess, feed_dict={phi:np.random.rand(256,2)})
+    shapes = [1]*len(theta_samples.shape)
+    shapes[0] = theta_samples.shape[0]
+    thetas = tf.tile(theta_samples, tf.stack(shapes))
+    # return theta_samples, tf.reduce_sum(thetas)
+    n_thetas = shape[0]
+    permutation = np.tile(np.arange(n_thetas), n_thetas)
+    permutation_idx = np.repeat(np.arange(n_thetas), n_thetas)
+    permutation, permutation_idx = non_equal(permutation, permutation_idx)
+    # import pdb; pdb.set_trace()
+
+    theta_shrunk = tf.gather(thetas, permutation)
+    theta_pemute = tf.gather(thetas, permutation_idx)
+    return theta_samples, tf.reduce_mean(theta_shrunk)
+    # theta_shrunk = tf.reshape(theta_shrunk, (n_thetas, n_thetas-1, *theta_shrunk.get_shape().as_list()[1:]))
+    # theta_pemute = tf.reshape(theta_pemute, (n_thetas, n_thetas-1, *theta_pemute.get_shape().as_list()[1:]))
+    # all_dist = tf.reduce_sum(tf.abs(theta_pemute - theta_shrunk), axis=2)
+    # nn_dist = tf.reduce_min(all_dist, axis=1)
+    # nn_mean = tf.reduce_mean(nn_dist)
+    # nn_mean.eval(session=sess, feed_dict={phi:np.random.rand(256,2)})
+    # return theta_samples, -nn_mean
+
+
+def rnd_pairwise_min_dist(phi, g, permutation, permutation_idx, n_dist, shape):
     op, params = g(phi)
     theta_samples = op[0]
-    theta_shrunk = tf.gather(theta_samples, permutation_idx)
-    theta_pemute = tf.gather(theta_samples, permutation)
+    theta_samples = tf.reshape(theta_samples, shape)
+    shapes = [1]*len(theta_samples.shape)
+    shapes[0] = theta_samples.shape[0]
+    thetas = tf.tile(theta_samples, tf.stack(shapes))
+    permutation = permutation[:n_dist]
+    permutation_idx = permutation_idx[:n_dist]
+    theta_shrunk = tf.gather(thetas, permutation_idx)
+    theta_pemute = tf.gather(thetas, permutation)
     diff = theta_pemute - theta_shrunk + EPS
     sqrdiff = tf.abs(diff)
     euclids = tf.reduce_sum(sqrdiff, reduction_indices=1) + EPS
@@ -79,11 +124,17 @@ def rnd_pairwise_min_dist(phi, g, permutation, permutation_idx):
     return theta_samples, -rp
 
 
-def rnd_pairwise_gap_ratio(phi, g, permutation, permutation_idx):
+def rnd_pairwise_gap_ratio(phi, g, permutation, permutation_idx, n_dist, shape):
     op, params = g(phi)
     theta_samples = op[0]
-    theta_shrunk = tf.gather(theta_samples, permutation_idx)
-    theta_pemute = tf.gather(theta_samples, permutation)
+    theta_samples = tf.reshape(theta_samples, shape)
+    shapes = [1]*len(theta_samples.shape)
+    shapes[0] = theta_samples.shape[0]
+    thetas = tf.tile(theta_samples, tf.stack(shapes))
+    permutation = permutation[:n_dist]
+    permutation_idx = permutation_idx[:n_dist]
+    theta_shrunk = tf.gather(thetas, permutation_idx)
+    theta_pemute = tf.gather(thetas, permutation)
     diff = theta_pemute - theta_shrunk + EPS
     sqrdiff = tf.abs(diff)
     euclids = tf.reduce_sum(sqrdiff, reduction_indices=1) + EPS
@@ -110,14 +161,21 @@ def train(sdf,
                         inp_shapes=[phi_shape],
                         out_shapes=[theta_shape],
                         **template_options)
-    theta_samples, loss1 = rnd_pairwise_min_dist(phi, g, permutation, permutation_idx)
+    n_dist = batch_size**2//4
+    theta_samples, loss1 = rnd_nn_mean_dist(phi, g, (batch_size, theta_ndim))
     loss2 = sdf(theta_samples)
     # loss = loss2
-    lmbda = 1.0
+    lmbda = 0.2
     loss2 = lmbda*loss2
     loss = loss1 + loss2
     # loss = loss1
     variables = tf.all_variables()
+    sess = tf.InteractiveSession()
+    init = tf.initialize_all_variables()
+    sess.run(init)
+    # import pdb; pdb.set_trace()
+
+    loss1.eval(session=sess, feed_dict={phi: np.random.rand(256, 2)})
     gradients1 = tf.gradients(loss1, variables)
     gradients2 = tf.gradients(loss2, variables)
     lossgradient = tf.gradients(loss, [loss1, loss2])
@@ -136,14 +194,14 @@ def train(sdf,
 def sumsum(xs):
     return np.sum([np.sum(x) for x in xs])
 
-def train_loop(loss, update_step, phi, permutation, permutation_idx, fetches, n_iterations=10000,
+def train_loop(loss, update_step, phi, permutation, permutation_idx, fetches, n_iterations=50000,
                batch_size=256):
     sess = tf.Session()
     init = tf.initialize_all_variables()
     sess.run(init)
     for i in range(n_iterations):
         phi_samples = np.random.rand(*phi.get_shape().as_list())
-        perm_data = np.arange(batch_size)
+        perm_data = np.arange(batch_size**2)
         np.random.shuffle(perm_data)
         non_iden_perm_data = non_iden(perm_data)
         perm_data_idx = non_iden_idx(perm_data)