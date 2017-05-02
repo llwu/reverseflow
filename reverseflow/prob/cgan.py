eecf111a8b28f32a9107d823c0d81c97f89dc4e2
Zenna Tavares <zennatavares@gmail.com>
2017-05-02 01:29:47 -0400

    fix bug in gan term

--- reverseflow/prob/cgan.py
+++ reverseflow/prob/cgan.py
@@ -2,8 +2,10 @@
 from arrows.config import floatX
 from tensorflow import Tensor
 import tensorflow as tf
-from wacacore.train.common import train_loop, updates
+from wacacore.train.common import (train_loop, updates, variable_summaries,
+  setup_file_writers)
 from wacacore.util.generators import infinite_samples
+from wacacore.train.callbacks import every_n, summary_writes
 from typing import Generator, Callable, Sequence
 import numpy as np
 from tflearn.layers import fully_connected
@@ -25,46 +27,64 @@ def tf_cgan(x_prior: Tensor,
     pcrv_inp: Poster Conditional Random Variable Outputs X
     discriminator: Tensor -> Tensor for discriminator function
   """
+  eps = 1e-6
   # 1. Attach the prior to its generator
   # Construct the two loss functiosns
   y = f(x_prior)
   x_fake = g(y, z)
 
   # Pipe the output of cgan into discriminator
-  real = disc(x_prior, False)
+  real = disc(x_prior, y, False) + eps
   # Pipe output of prior into discriminator
-  fake = disc(x_fake, True)
+  fake = disc(x_fake, y, True) - eps
 
-  loss_d = tf.reduce_mean(-tf.log(real) + tf.log(1 - fake))
+  loss_d = tf.reduce_mean(-tf.log(real) - tf.log(1 - fake))
   loss_g = tf.reduce_mean(-tf.log(fake))
   losses = [loss_d, loss_g]
 
   # Fetch
   fetch = {'losses': losses}
-  fetch['check'] = tf.add_check_numerics_ops()
+  # fetch['check'] = tf.add_check_numerics_ops()
+  fetch['real'] = real[0]
+  fetch['fake'] = fake[0]
 
-  # Make loss updates from losses
   # 1st element from update is update tensor, 0th is optimizer
+  # Make loss updates from losses
   g_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator')
   d_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='discriminator')
-  import pdb; pdb.set_trace()
   loss_updates = [updates(loss_d, d_vars, options)[1],
                   updates(loss_g, g_vars, options)[1]]
 
+  loss_ratios = [1, 3]
   def generator():
     while True:
-      yield {x_prior:next(x_prior_gen),
+      yield {x_prior: next(x_prior_gen),
              z: next(z_gen)}
 
   train_generators = [generator()]
 
   # FIXME: Hyperparameterize this
-  loss_ratios = None
 
   # Init
   sess = tf.Session()
   init = tf.initialize_all_variables()
   sess.run(init)
+
+  # Reconstruction loss
+  y_recon = f(x_fake)
+  recon_loss = tf.reduce_mean(tf.abs(y_recon - y))
+  fetch['recon_loss'] = recon_loss
+
+  # Summaries
+  summaries = variable_summaries({'gen_loss': loss_g,
+                                  'disc_loss': loss_d,
+                                  'recon_loss': recon_loss})
+
+  ss = tf.summary.histogram("y_recon", y_recon)
+  fetch['summaries'] = summaries
+  writers = setup_file_writers('summaries', sess)
+  options['writers'] = writers
+  callbacks = [every_n(summary_writes, 25)]
 
   train_loop(sess,
              loss_updates=loss_updates,
@@ -74,7 +94,7 @@ def tf_cgan(x_prior: Tensor,
              loss_ratios=loss_ratios,
              test_every=100,
              num_iterations=100000,
-             callbacks=None,
+             callbacks=callbacks,
              **options)
 
 
@@ -93,9 +113,9 @@ def main():
     """The model"""
     return tf.reduce_sum(x, axis=1)
 
-  z_len = 2
+  z_len = 1
   z = tf.placeholder(dtype=floatX(), shape=(batch_size, z_len))
-  z_gen = infinite_samples(np.random.randn,
+  z_gen = infinite_samples(np.random.rand,
                            shape=(z_len,),
                            batch_size=batch_size,
                            add_batch=True)
@@ -106,25 +126,46 @@ def main():
       with tf.variable_scope("generator"):
         y_exp = tf.expand_dims(y, 1)
         inp = tf.concat([y_exp, z], axis=1)
-        out = fully_connected(inp, x_len, activation='relu')
+        inp = fully_connected(inp, 3, activation='elu')
+        out = fully_connected(inp, x_len, activation='elu')
         return out
 
+  def g_pi(y, z):
+    """Parametric Inverse Generator"""
+    with tf.name_scope("generator"):
+      with tf.variable_scope("generator"):
+        theta_len = 1
+        # the neural network will take as input z, and output
+        # the two parameters for
+        inp = z
+        inp = fully_connected(inp, 3, activation='elu')
+        theta = fully_connected(inp, theta_len, activation='elu')
+        x_1 = tf.expand_dims(y, 1) - theta
+        x_2 = theta
+        x = tf.concat([x_1, x_2], 1)
+        return x
 
-  def disc(x, reuse):
+
+  def disc(x, y, reuse, use_y=True):
     """Discriminator"""
     with tf.name_scope("discriminator"):
       with tf.variable_scope("discriminator", reuse=reuse):
-        inp = x
+        if use_y:
+          inp = tf.concat([x, tf.expand_dims(y, 1)], 1)
+        else:
+          inp = x
+        # import pdb; pdb.set_trace()
+        inp = fully_connected(inp, 3, activation='elu')
         out = fully_connected(inp, 1, activation='sigmoid')
         return out
 
-  options = {'update': 'adam', 'learning_rate': 0.005}
+  options = {'update': 'adam', 'learning_rate': 0.001, 'save': True}
   tf_cgan(x_prior,
           x_prior_gen,
           z,
           z_gen,
           f,
-          g,
+          g_pi,
           disc,
           options)
 
